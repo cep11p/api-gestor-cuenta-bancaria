150f5197da6494c7b9a7bb897157c68fccda2ecf
carlos <cep11p@gmail.com>
2020-09-04 13:30:40 -0300

    TG-39 - TG-49 #Closed Se modifica la documentacionJson  https://tree.taiga.io/project/cep11p-gcb/us/39?milestone=272563

--- documentacionJson/SubSucursales.php
+++ documentacionJson/SubSucursales.php
@@ -12,7 +12,8 @@
         "codigo_postal": "8328",
         "codigo": "161014",
         "sucursalid": 14,
-        "nombre": "Allen (Suc. Allen)"
+        "nombre": "Allen (Suc. Allen)",
+        "sucursal_codigo": "265"
     },
     {
         "id": 2,
@@ -20,7 +21,8 @@
         "codigo_postal": "8400",
         "codigo": "161399",
         "sucursalid": 3,
-        "nombre": "Bariloche (Suc. Bariloche)"
+        "nombre": "Bariloche (Suc. Bariloche)",
+        "sucursal_codigo": "255"
     },
     {
         "id": 3,
@@ -28,7 +30,8 @@
         "codigo_postal": "8412",
         "codigo": "161355",
         "sucursalid": 3,
-        "nombre": "Pilcaniyeu (Suc. Bariloche)"
+        "nombre": "Pilcaniyeu (Suc. Bariloche)",
+        "sucursal_codigo": "255"
     },
     {
         "id": 4,
@@ -36,7 +39,8 @@
         "codigo_postal": "8364",
         "codigo": "161127",
         "sucursalid": 1,
-        "nombre": "C. Belisle (Suc. Cinco Saltos)"
+        "nombre": "C. Belisle (Suc. Cinco Saltos)",
+        "sucursal_codigo": "256"
     }
  * ]
 */
