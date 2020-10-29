2aa1f3ecf5104bc505805dae262d04339341f110
carlos <cep11p@gmail.com>
2020-10-29 00:16:14 -0300

    TG-205 Se avanza en el creado del archivo interbanking .txt https://tree.taiga.io/project/cep11p-gcb/us/205

--- models/Interbanking.php
+++ models/Interbanking.php
@@ -31,14 +31,14 @@ class Interbanking extends Model
      */
     public static function exportar($params){
         $cuentas = [
-                '2                      DDDDDDDDDDDDDDDDDDDDDDDDDDDDDSNNTTTTTTTTTTTXXXXXXXXXXXXXXXXXXXXXXRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR',
-                '2                      DDDDDDDDDDDDDDDDDDDDDDDDDDDDDSNNTTTTTTTTTTTXXXXXXXXXXXXXXXXXXXXXXRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR',
-                '2                      DDDDDDDDDDDDDDDDDDDDDDDDDDDDDSNNTTTTTTTTTTTXXXXXXXXXXXXXXXXXXXXXXRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR',
-                '2                      DDDDDDDDDDDDDDDDDDDDDDDDDDDDDSNNTTTTTTTTTTTXXXXXXXXXXXXXXXXXXXXXXRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR',
+                ['id'=>1],
+                ['id'=>2],
+                ['id'=>3],
+                ['id'=>5],
             ];
         $model = new Interbanking();
         $model->setAttributes($params);
-        $model->getInterbakingTxt($prestaciones);
+        $model->getInterbakingTxt($cuentas);
         
          
         die();
@@ -47,12 +47,47 @@ class Interbanking extends Model
     public function getInterbakingTxt($params) {
         $interbankin_txt = '1'.$this->codigo_cliente."\n";
         $final_txt = '3'.$this->codigo_cliente.str_pad(count($params), 6, "0", STR_PAD_LEFT);
+        
+        //obtenemos los datos del propietario de cada cuenta
+        $datos = $this->getDatosCuentas($params);
+        
+        print_r($datos);
+        die();
+        
         foreach ($params as $value) {
             $interbankin_txt .=$value."\n";
         }
         $interbankin_txt .= $final_txt;
         
         print_r($interbankin_txt);die();
+    }
+    
+    /**
+     * Instanciamos los datos del propientario de la cuenta desde una lista de cuentas
+     * @param array $params Lista de cuentas
+     * @throws \yii\web\HttpException
+     * @return array Se devuelve una lista de cuenta con los datos del propietario
+     */
+    public function getDatosCuentas($params) {
+        $resultado = [];
+        $cuentaSearch = new CuentaSearch();
+        $cuenta_ids='';
+        foreach ($params as $value) {
+            //buscamos cuentas por lista de ids
+            $cuenta_ids .= (empty($cuenta_ids))?$value['id']:','.$value['id'];
+        }
+        
+        
+        //obtenemos la lista de cuentas con sus datos
+        $lista_cuenta = $cuentaSearch->search(['ids'=>$cuenta_ids]);
+        
+        if(empty($lista_cuenta)){
+            throw new \yii\web\HttpException(400, 'Lista de cuenta vacia');
+        }
+        
+        $resultado = Cuenta::getCuentaYCuil($lista_cuenta);
+        
+        return $resultado;
     }
     
     
