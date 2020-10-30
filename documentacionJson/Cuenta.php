043bb066d98a338c34b41aa546738b639787bc97
carlos <cep11p@gmail.com>
2020-10-30 14:20:01 -0300

    TG-214 Se actualiza la documentacionJson https://tree.taiga.io/project/cep11p-gcb/us/214

--- documentacionJson/Cuenta.php
+++ documentacionJson/Cuenta.php
@@ -4,48 +4,151 @@
 /**
 * @url http://api.gcb.local/cuentas
 * @url http://api.gcb.local/cuentas?ids=1,2,3 url con filtrado de ids
+* @url http://api.gcb.local/cuentas?global_param=27351545475 url con filtro cuil, nombre, apellido o nro de documento
 * @method GET
 * @arrayReturn
-[
-    {
-        "id": 1,
-        "cbu": "340220908126708871006",
-        "personaid": 1,
-        "bancoid": 1,
-        "tipo_cuentaid": 1,
-        "create_at": "2020-10-16 16:10:43",
-        "banco": "Patagonia",
-        "tipo_cuenta": "cuenta corriente",
-        "cuil": "20238512669",
-        "apellido": "González",
-        "nombre": "Victoria Margarita"
-    },
-    {
-        "id": 2,
-        "cbu": "340250608122051844009",
-        "personaid": 2,
-        "bancoid": 1,
-        "tipo_cuentaid": 1,
-        "create_at": "2020-10-16 16:10:43",
-        "banco": "Patagonia",
-        "tipo_cuenta": "cuenta corriente",
-        "cuil": "20320542389",
-        "apellido": "Rodríguez",
-        "nombre": "Isabel Sofía"
-    },
-    {
-        "id": 3,
-        "cbu": "340250608122051855003",
-        "personaid": 3,
-        "bancoid": 1,
-        "tipo_cuentaid": 1,
-        "create_at": "2020-10-16 16:10:43",
-        "banco": "Patagonia",
-        "tipo_cuenta": "cuenta corriente",
-        "cuil": "20284145559",
-        "apellido": "Gómez",
-        "nombre": "Dulce María"
-    }
+{
+    "pagesize": 20,
+    "pages": 3,
+    "total_filtrado": 59,
+    "resultado": [
+        {
+            "id": 1,
+            "cbu": "0340220908126708871006",
+            "personaid": 102,
+            "bancoid": 1,
+            "tipo_cuentaid": 1,
+            "create_at": "2020-10-29 14:10:34",
+            "tesoreria_alta": 1,
+            "banco": "Patagonia",
+            "tipo_cuenta": "Cuenta Corriente",
+            "apellido": "Marileo",
+            "nombre": "Romina Azucena",
+            "cuil": "27351545475",
+            "nro_documento": "35154547",
+            "telefono": "",
+            "celular": "",
+            "lugar": {
+                "id": 464,
+                "nombre": "",
+                "calle": "hipolito yrigoyen",
+                "altura": "771",
+                "localidadid": 2576,
+                "latitud": "",
+                "longitud": "",
+                "barrio": "",
+                "piso": "",
+                "depto": "",
+                "escalera": "",
+                "entre_calle_1": "",
+                "entre_calle_2": "",
+                "localidad": "General Roca",
+                "codigo_postal": 8332
+            }
+        },
+        {
+            "id": 2,
+            "cbu": "0340250608122051844009",
+            "personaid": 103,
+            "bancoid": 1,
+            "tipo_cuentaid": 1,
+            "create_at": "2020-10-29 14:10:34",
+            "tesoreria_alta": 1,
+            "banco": "Patagonia",
+            "tipo_cuenta": "Cuenta Corriente",
+            "apellido": "Benitez",
+            "nombre": "Celinda",
+            "cuil": "27113457886",
+            "nro_documento": "11345788",
+            "telefono": "",
+            "celular": "",
+            "lugar": {
+                "id": 465,
+                "nombre": "",
+                "calle": "belgrano",
+                "altura": "1781",
+                "localidadid": 2640,
+                "latitud": "",
+                "longitud": "",
+                "barrio": "",
+                "piso": "",
+                "depto": "",
+                "escalera": "",
+                "entre_calle_1": "",
+                "entre_calle_2": "",
+                "localidad": "Viedma",
+                "codigo_postal": 8500
+            }
+        },
+        {
+            "id": 3,
+            "cbu": "0340250608122051855003",
+            "personaid": 104,
+            "bancoid": 1,
+            "tipo_cuentaid": 1,
+            "create_at": "2020-10-29 14:10:34",
+            "tesoreria_alta": 1,
+            "banco": "Patagonia",
+            "tipo_cuenta": "Cuenta Corriente",
+            "apellido": "Sacco",
+            "nombre": "Ruben Dalmiro",
+            "cuil": "20123827679",
+            "nro_documento": "12382767",
+            "telefono": "",
+            "celular": "",
+            "lugar": {
+                "id": 466,
+                "nombre": "",
+                "calle": "mendoza",
+                "altura": "360",
+                "localidadid": 2640,
+                "latitud": "",
+                "longitud": "",
+                "barrio": "",
+                "piso": "",
+                "depto": "",
+                "escalera": "",
+                "entre_calle_1": "",
+                "entre_calle_2": "",
+                "localidad": "Viedma",
+                "codigo_postal": 8500
+            }
+        },
+        {
+            "id": 5,
+            "cbu": "0340250608122040842018",
+            "personaid": 105,
+            "bancoid": 1,
+            "tipo_cuentaid": 1,
+            "create_at": "2020-10-29 14:10:34",
+            "tesoreria_alta": 0,
+            "banco": "Patagonia",
+            "tipo_cuenta": "Cuenta Corriente",
+            "apellido": "Cabrera",
+            "nombre": "Abel A",
+            "cuil": "20146434682",
+            "nro_documento": "14643468",
+            "telefono": "",
+            "celular": "",
+            "lugar": {
+                "id": 467,
+                "nombre": "",
+                "calle": "esperanza",
+                "altura": "26",
+                "localidadid": 2640,
+                "latitud": "",
+                "longitud": "",
+                "barrio": "",
+                "piso": "",
+                "depto": "",
+                "escalera": "",
+                "entre_calle_1": "",
+                "entre_calle_2": "",
+                "localidad": "Viedma",
+                "codigo_postal": 8500
+            }
+        }
+    ]
 ]
 */
 
