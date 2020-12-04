e7d33457c01aefd2e4aed8f8eece997f6b983a32
cep11p <cep11p@gmail.com>
2020-12-04 09:34:06 -0300

    TG-262 - TG-269 #Closed Se crea la documentacion de importado de persona con su CBU https://tree.taiga.io/project/cep11p-gcb/us/262

---
+++ documentacionJson/Herramienta.php
@@ -0,0 +1,10 @@
+<?php
+
+/**** Se importa una lista de personas con su CBU ****/
+/**
+* Se lee un archivo .xlsx para caputar los datos e imnportar  personas con su CBU
+* @url http://api.gcb.local/herramienta/importar
+* @method GET (form-data)
+* @arrayReturn
+*/
+
