6525d43a34925c6389c386c40ea194f84b6bb26b
cep11p <cep11p@gmail.com>
2021-02-24 11:25:18 -0300

    TG-322 - TG-340 #Closed Se realiza la baja de usuario, se actualiza la documentacionJson y se configura el accessControl https://tree.taiga.io/project/cep11p-gcb/us/322

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -70,8 +70,8 @@ class UsuarioController extends ActiveController
                 ],
                 [
                     'allow' => true,
-                    'actions' => ['index','create','view','buscar-persona-por-cuil'],
-                    'roles' => ['@'],
+                    'actions' => ['index','create','view','buscar-persona-por-cuil','baja'],
+                    'roles' => ['soporte'],
                 ]
             ]
         ];
@@ -187,6 +187,35 @@ class UsuarioController extends ActiveController
             $resultado['success'] = false;
         }        
 
+        return $resultado;
+    }
+
+    /**
+     * Esta funcion habilita y deshabilita un usuario
+     *
+     * @param [int] $id
+     * @return void
+     */
+    public function actionBaja($id){
+        $params = Yii::$app->request->post();
+        
+        $model = User::findOne(['id'=>$id]);            
+        if($model==NULL){
+            throw new \yii\web\HttpException(400, 'El usuario con el id '.$id.' no existe!');
+        }
+        
+        if($params['baja']===true){
+            $resultado['message'] = 'Se inhabilita el usuario correctamente.';
+            if(!$model->setBaja($params)){
+                $resultado['message'] = 'No se pudo inhabilitar el usuario correctamente';
+            }
+        }else if($params['baja']===false){
+            $resultado['message'] = 'Se Habilita el usuario correctamente.';
+            if(!$model->unSetBaja($params)){
+                $resultado['message'] = 'No se pudo habilitar el usuario correctamente';
+            }
+        }
+        
         return $resultado;
     }
 
--- documentacionJson/Usuario.php
+++ documentacionJson/Usuario.php
@@ -22,20 +22,108 @@
 }
 **/
 
-/**** Para modificar*****
-* @url http://api.gestor-inventario.local/proveedors/{$id} 
-* @method PUT
+
+
+/*****Para crear usuario****
+* @url http://api.gcb.local/usuarios 
+* @method POST
 * @param arrayJson
+# Con persona existente
+{
+	"personaid":2,
+	"usuario":{
+		"personaid":2,
+		"username":"cep11p",
+		"password":"carlos",
+		"email":"cep11p@correo.com",
+		"localidadid":2626,
+		"rol":"soporte"
+	}
+}
+# Con persona nueva
+{
+	"nombre":"Carlos",
+	"apellido":"Peralta",
+	"nro_documento":"36123456",
+	"cuil":"20361234569",
+	"usuario":{
+		"username":"cperez",
+		"password":"carlos",
+		"email":"cperez@correo.com",
+		"localidadid":2626,
+		"rol":"soporte"
+	}
+}
 **/
 
 /****** Para visualizar*****
-* @url http://api.gestor-inventario.local/proveedors/{$id} 
+* @url http://api.gcb.local/usuarios/2
 * @method GET
 * @return arrayJson
+{
+    "id": 2,
+    "username": "admin",
+    "email": "admin@correo.com",
+    "confirmed_at": 1556894840,
+    "unconfirmed_email": null,
+    "blocked_at": null,
+    "registration_ip": "172.18.0.2",
+    "created_at": 1556894840,
+    "updated_at": 1607700159,
+    "flags": 0,
+    "last_login_at": 1610453141,
+    "personaid": 1,
+    "fecha_baja": "",
+    "baja": false,
+    "descripcion_baja": "",
+    "localidadid": 2626,
+    "nombre": "Victoria Margarita",
+    "apellido": "González",
+    "nro_documento": "23851266",
+    "cuil": "20068512669",
+    "localidad": "Rio Colorado"
+}
 */
 
-/****** Para borrar una localidad *****
-* @url http://api.gestor-inventario.local/proveedors/{$id} 
-* @method Delete
-* @return arrayJson
-*/
+/**** Dar de baja un Usuarios*****
+* @url http://api.gcb.local/usuarios/baja/47 
+* @method PUT
+* @param arrayJson
+{
+  "baja":true,
+	"descripcion_baja":"Esto es una descripcion de baja de usuario"
+}
+**/
+
+/** Buscar a un usuario por nro de cuil
+ * @url http://api.gcb.local/usuarios/buscar-persona-por-cuil/20320542389
+ * @method GET
+ * @return arrayJson
+ * 
+ {
+    "success": true,
+    "resultado": {
+        "id": 2,
+        "nro_documento": "32054238",
+        "cuil": "20320542389",
+        "nombre": "Isabel Sofía",
+        "apellido": "Rodríguez",
+        "usuario": {
+            "id": 13,
+            "username": "cep11p",
+            "email": "cep11p@correo.com",
+            "auth_key": "aN1ar_QzmaG90RK8vGo3IQdwI6ylIPo3",
+            "confirmed_at": 1614092528,
+            "unconfirmed_email": null,
+            "blocked_at": null,
+            "registration_ip": "172.20.0.8",
+            "created_at": 1614092528,
+            "updated_at": 1614092528,
+            "flags": 0,
+            "last_login_at": null,
+            "personaid": 2,
+            "localidadid": 2626
+        }
+    }
+  }
+ **/
