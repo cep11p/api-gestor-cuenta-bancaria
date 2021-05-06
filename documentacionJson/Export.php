8682b1c0458799534ec76b0eaaf1b69bf23a9925
cep11p <cep11p@gmail.com>
2021-05-06 12:05:35 -0300

    TG-369 - TG-377 #Closed Se crea la documentacion Json https://tree.taiga.io/project/cep11p-gcb/us/369

---
+++ documentacionJson/Export.php
@@ -0,0 +1,29 @@
+<?php
+
+/**** Para mostrar listado ****/
+/**
+* @url http://api.gcb.local/exports
+* @url http://api.gcb.local/exports?sort=-export_at se ordena la lista por fecha de exportacion
+* @method GET
+* @arrayReturn
+{
+    "pagesize": 10,
+    "pages": 1,
+    "total_filtrado": 1,
+    "resultado": [
+        {
+            "id": 1,
+            "lista_ids": "1,2,3",
+            "tipo": "interbanking",
+            "export_at": "2021-05-06 14:24:21"
+        },
+        {...},
+        {
+            "id": 1,
+            "lista_ids": "1,2,3",
+            "tipo": "interbanking",
+            "export_at": "2021-05-06 14:24:21"
+        }
+    ]
+}
+*/
