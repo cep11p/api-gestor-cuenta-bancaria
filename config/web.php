0da0002831ce1710a727fa64597b7edf9110b6c6
cep11p <cep11p@gmail.com>
2021-09-15 13:30:26 -0300

    Soluciona un bug! No se podia registrar una rule por un tema relacionado con el preg_match, se sobre escribe una clase para solucionar el problema

--- config/web.php
+++ config/web.php
@@ -2,7 +2,7 @@
 
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
-
+Yii::$classMap['dektrium\rbac\models\Rule'] = '@app/overrides/Rule.php';
 $config = [
     'id' => 'basic',
     'name' => 'Gestor de cuentas bancarias',
